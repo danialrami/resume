09bedfd7a0ba972e30c0227a7d2ff83988ebb226
Daniel Ramirez <daniel@danialrami.com>
2026-07-14 20:20:44 -0500

    Production build 2026-07-14 20:20:44

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-RESUME_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+RESUME_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$RESUME_DIR"
 
 echo "Setting up resume repository..."
